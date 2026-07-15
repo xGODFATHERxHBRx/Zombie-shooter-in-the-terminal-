637cdbe8f6b79f07541228cac5643066197d48c8
xGODFATHERx <hasibraihan523@gmail.com>
2026-07-15 18:43:44 +0600

    Fix install.sh download URL

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ set -e
 echo "🧟 Installing Zombie Shooter..."
 
 # Download the .deb package
-wget -O /tmp/zombie-shooter.deb https://github.com/xGODFATHERxHBRx/Zombie-shooter-in-the-terminal-/raw/main/zombie-shooter_1.0_amd64.deb
+wget -O /tmp/zombie-shooter.deb https://raw.githubusercontent.com/xGODFATHERxHBRx/Zombie-shooter-in-the-terminal-/main/zombie-shooter_1.0_amd64.deb
 
-# Install it
+# Intall it
 sudo apt install /tmp/zombie-shooter.deb -y
 
 # Clean up
